f6aede87d9d42222ba590e0a60731f7220ecefa4
zachhuang4026 <zachhuang4026@gmail.com>
2022-12-03 18:40:10 -0600

    add database

--- src/main/java/edu/uchicago/zachhuang4026/quarkus/Resources/ObjectResources.java
+++ src/main/java/edu/uchicago/zachhuang4026/quarkus/Resources/ObjectResources.java
@@ -35,6 +35,22 @@ public class ObjectResources {
         return Response.ok().entity(successResponse).build();
     }
 
+    @POST
+    @Path("/multiple")
+    public Response addMultiple(List<Object> objects) {
+        List<Object> newObjects;
+
+        try {
+            newObjects = objectService.addMultiple(objects);
+        } catch (Exception e) {
+            ItemResponse errorResponse = new ItemResponse("204", null);
+            return Response.status(Response.Status.NO_CONTENT).entity(errorResponse).build();
+        }
+
+        ItemResponses successResponse = new ItemResponses("200", newObjects);
+        return Response.ok().entity(successResponse).build();
+    }
+
     @GET
     @Path("/{id}")
     public Response get(@PathParam("id") String id) {
